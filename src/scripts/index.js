e5efeaf61c0508169e9da745f9dfa9bb9a47d0fe
Farouq <farouq006@gmail.com>
2025-11-22 16:42:29 +0300

    Add custom cursor/pointer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,11 +2,15 @@ import Ship from './classes/ship.js';
 import Gameboard from './classes/gameboard.js';
 import Player from './classes/Player.js';
 import "../css/main.css";
+import { gameLoader } from './loaders/game.js';
 
 const starter = document.getElementById('starter');
 const playerNameForm = document.querySelector('form');
+const playerNameInput = document.getElementById('player-name-input');
 
 playerNameForm.addEventListener('submit', e => {
     e.preventDefault();
+    const name = playerNameInput.innerText;
     starter.style.display = 'none';
+    gameLoader(name);
 })
---
+++ src/scripts/loaders/game.js
@@ -0,0 +1,3 @@
+export function gameLoader(name) {
+    
+}
